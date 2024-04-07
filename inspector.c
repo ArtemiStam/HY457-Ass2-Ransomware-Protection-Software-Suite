1dcf1355ecfcdd140e7bc2158c8d8433cc8a6796
ArtemiStam <artemistamataki@gmail.com>
2024-04-07 15:58:43 +0300

    Last commit before different approach on getting strings from a binary

--- inspector.c
+++ inspector.c
@@ -3,6 +3,7 @@
 
 int inspection_scan(char **file_array, const int file_num, char ***str_array, char ***paths_to_strings) {
     int i = 0, j = 0, total_strs = 0, str_num = 0;
+    char *addresses;
 
     if (file_array == NULL || str_array == NULL || paths_to_strings == NULL) {
         status_update(1, "Invalid file");
@@ -12,6 +13,7 @@ int inspection_scan(char **file_array, const int file_num, char ***str_array, ch
     
     for (i = 0; i < file_num; i++) /*Get the strings from every file*/
     {
+        printf("%s\n", file_array[i]);
         if ((str_num = extract_strings(file_array[i], str_array, total_strs)) > 0) /*add strings to str_array*/
         {
             total_strs = total_strs + str_num;
@@ -27,6 +29,8 @@ int inspection_scan(char **file_array, const int file_num, char ***str_array, ch
             {
                 (*paths_to_strings)[j] = file_array[i];
             }
+
+
         }
     }
 
@@ -37,9 +41,9 @@ int inspection_scan(char **file_array, const int file_num, char ***str_array, ch
 int extract_strings(const char *file, char ***str_array, int total_strs) {
     FILE *fp;
     char *buffer;
-    //char *string;
+    char *string;
     int i = 0, bytes_in_file = 0, bytes_read = 0;
-    int /*tail = 0,*/ printable_chars = 0, str_num = 0;
+    int tail = 0, printable_chars = 0, str_num = 0;
 
     if (file == NULL) {
         status_update(1, "Invalid file");
@@ -83,22 +87,24 @@ int extract_strings(const char *file, char ***str_array, int total_strs) {
     }
     fclose(fp);
 
-    //tail = 0;
+    tail = 0;
     for (i = 0; i < bytes_in_file; i++)
     {
         if ((buffer[i] <= 126) && (buffer[i] >= 32))
         {
-            /*if (i == 0)
+            printf("HERE\n");
+            if (i == 0)
             {
                 tail = -1;
-            }*/
+            }
         
             printable_chars++;
-            /*if (i == bytes_in_file-1 && tail != -1)
+            if (i == bytes_in_file-1 && tail != -1)
             {
                 if (i - tail >= 4) //for it to be a domain it must be at least 4 characters long a.gr
                 {
                     str_num++;
+                    //printf("%d", i - tail + 1);
                     string  = (char *) malloc(i - tail + 1);
                     if (string == NULL)
                     {
@@ -109,22 +115,30 @@ int extract_strings(const char *file, char ***str_array, int total_strs) {
                     memcpy(string, buffer + tail + 1, i - tail); //using pointer arithmetic to start copying from the desired index
                     string[i - tail + 1] = '\0';
 
-                    *str_array = realloc(*str_array, ++total_strs);
+                    (*str_array)[total_strs++] = string;
+                    *str_array = (char **)realloc(*str_array, sizeof(char *) *(total_strs+1)); //Add to the str_array a place for an additional string
                     if (*str_array == NULL)
                     {
                         status_update(1, "Memory Reallocation Failed");
                         status_update(1, "Application Ended");
                         exit(1);
                     }
-                    (*str_array)[total_strs-1] = string;
+                    /**str_array = realloc(*str_array, sizeof(char*)*++total_strs);
+                    if (*str_array == NULL)
+                    {
+                        status_update(1, "Memory Reallocation Failed");
+                        status_update(1, "Application Ended");
+                        exit(1);
+                    }
+                    (*str_array)[total_strs-1] = string;*/
                 }
             }
-            
         } else {
             if (tail == -1)
             {
                 if (i >= 4) //for it to be a domain it must be at least 4 characters long a.gr
                 {
+                    //printf("%d", i - tail + 1);
                     str_num++;
                     string = (char *)malloc(i + 1);
                     if (string == NULL)
@@ -136,19 +150,20 @@ int extract_strings(const char *file, char ***str_array, int total_strs) {
                     memcpy(string, buffer, i);
                     string[i + 1] = '\0';
                      
-                    *str_array = realloc(*str_array, ++total_strs); //Add to the str_array a place for an additional string
+                    (*str_array)[total_strs++] = string;
+                    *str_array = (char **)realloc(*str_array, sizeof(char *) *(total_strs+1)); //Add to the str_array a place for an additional string
                     if (*str_array == NULL)
                     {
                         status_update(1, "Memory Reallocation Failed");
                         status_update(1, "Application Ended");
                         exit(1);
                     }
-                    (*str_array)[total_strs-1] = string;
                 }
             }
             else {
                 if (i - tail - 1 >= 4) //for it to be a domain it must be at least 4 characters long a.gr
                 {
+                    fprintf(stderr, "%d", i - tail);
                     str_num++;
                     string = malloc(i - tail);
                     if (string == NULL)
@@ -159,18 +174,26 @@ int extract_strings(const char *file, char ***str_array, int total_strs) {
                     }
                     memcpy(string, buffer + tail + 1, i - tail - 1); //using pointer arithmetic to start copying from the desired index
                     string[i - tail] = '\0';
-                     
-                    *str_array = realloc(*str_array, ++total_strs); //Add to the str_array a place for an additional string
+
+                    (*str_array)[total_strs++] = string;
+                    *str_array = (char **)realloc(*str_array, sizeof(char *) *(total_strs+1)); //Add to the str_array a place for an additional string
                     if (*str_array == NULL)
                     {
                         status_update(1, "Memory Reallocation Failed");
                         status_update(1, "Application Ended");
                         exit(1);
                     }
-                    (*str_array)[total_strs-1] = string;
+                    /**str_array = realloc(*str_array, ++total_strs); //Add to the str_array a place for an additional string
+                    if (*str_array == NULL)
+                    {
+                        status_update(1, "Memory Reallocation Failed");
+                        status_update(1, "Application Ended");
+                        exit(1);
+                    }
+                    (*str_array)[total_strs-1] = string;*/
                 }
             }
-            tail = i;*/
+            tail = i;
         } 
     } 
 
